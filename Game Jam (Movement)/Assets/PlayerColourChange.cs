4e4d17f221d9dc3863413dd0eb39d66e7de9ed98
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-09-06 12:05:52 -0400

    Project and player
    Unity Project containing basic player sprite, and player controls

---
+++ Game Jam (Movement)/Assets/PlayerColourChange.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColourChange : MonoBehaviour
+{
+    Color[] colours = {Color.white, Color.yellow, Color.green, Color.red, Color.blue, Color.black};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+
+            Color temp = GetComponent<SpriteRenderer>().material.color;
+            int cc = 0;
+
+            if (colours[0] == temp)
+            {
+                gameObject.GetComponent<Renderer>().material.color = colours[5];
+            }
+            else
+            {
+                do
+                {
+
+                    if (colours[cc] == temp)
+                    {
+                        gameObject.GetComponent<Renderer>().material.color = colours[cc - 1];
+                        cc = 6;
+                    }
+                    else
+                    {
+                        cc++;
+                    }
+
+                } while (cc <= 5);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            Color temp = GetComponent<SpriteRenderer>().material.color;
+            int cc = 0;
+
+            if (colours[5] == temp)
+            {
+                gameObject.GetComponent<Renderer>().material.color = colours[0];
+            }
+            else
+            {
+                do
+                {
+
+                    if (colours[cc] == temp)
+                    {
+                        gameObject.GetComponent<Renderer>().material.color = colours[cc + 1];
+                        cc = 6;
+                    }
+                    else
+                    {
+                        cc++;
+                    }
+
+                } while (cc <= 5);
+            }
+        }
+    }
+}
---
+++ Game Jam (Movement)/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
