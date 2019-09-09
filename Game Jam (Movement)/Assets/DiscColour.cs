808af3572a3edeb6329ca7072b7770b7607c0413
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-09-09 11:22:43 -0400

    Integrated Disc
    I intergrated the disk scripts and sprites also i made them have a random colour on spawn, now working on colision

---
+++ Game Jam (Movement)/Assets/DiscColour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscColour : MonoBehaviour
+{
+    Color[] DC = { Color.white, Color.yellow, Color.green, Color.red, Color.blue, Color.black };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rng = Random.Range(0, 5);
+        gameObject.GetComponent<Renderer>().material.color = DC[rng];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Jam (Movement)/Assets/PlayerColition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameObject.GetComponent<BoxCollider2D>().Equals(true)) {
+            
+        }
+    }
+}
