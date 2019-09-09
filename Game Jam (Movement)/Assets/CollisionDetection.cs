a165c22e6e6fa00f77430528e70974ac230d085d
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-09-09 17:39:25 -0400

    Collision Detection (working?)

---
+++ Game Jam (Movement)/Assets/CollisionDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject colidedobject = GameObject.Find(collision.gameObject.name);
+        GameObject Player = GameObject.Find("Player");
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (colidedobject.GetComponent<Renderer>().material.color == Player.gameObject.GetComponent<Renderer>().material.color)
+        {
+
+
+        }
+        if(colidedobject.GetComponent<Renderer>().material.color != Player.gameObject.GetComponent<Renderer>().material.color)
+        {
+            GameObject ui = GameObject.Find("Main Camera");
+            ui.GetComponent<HealthBar>().damage = -20;
+        }
+    }
+}
--- Game Jam (Movement)/Assets/HealthBar.cs
+++ Game Jam (Movement)/Assets/HealthBar.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth = 100;
+    public int curHealth = 100;
+
+    public Texture2D bgImage;
+    public Texture2D fgImage;
+
+    public float healthBarLength;
+
+    // Use this for initialization
     void Start()
     {
-        
+        healthBarLength = Screen.width / 2;
     }
 
-    public void HealthDown() {
+    public int damage = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        AddjustCurrentHealth(damage);
+        damage = 0;
+    }
 
+    void OnGUI()
+    {
+        // Create one Group to contain both images
+        // Adjust the first 2 coordinates to place it somewhere else on-screen
+        GUI.BeginGroup(new Rect(0, 0, healthBarLength, 32));
+
+        // Draw the background image
+        GUI.Box(new Rect(0, 0, healthBarLength, 32), bgImage);
+
+        // Create a second Group which will be clipped
+        // We want to clip the image and not scale it, which is why we need the second Group
+        GUI.BeginGroup(new Rect(0, 0, curHealth / maxHealth * healthBarLength, 32));
+
+        // Draw the foreground image
+        GUI.Box(new Rect(0, 0, healthBarLength, 32), fgImage);
+
+        // End both Groups
+        GUI.EndGroup();
+
+        GUI.EndGroup();
+    }
+
+    public void AddjustCurrentHealth(int adj)
+    {
+
+        curHealth += adj;
+
+        if (curHealth < 0)
+            curHealth = 0;
+
+        if (curHealth > maxHealth)
+            curHealth = maxHealth;
+
+        if (maxHealth < 1)
+            maxHealth = 1;
+
+        healthBarLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
     }
 }
