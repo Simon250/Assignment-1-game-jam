6667750eda8518569fa1d3bfaf86abc74ca0773e
Joseph Smyth <54866635+JosephSmythy@users.noreply.github.com>
2019-09-08 22:18:58 -0400

    Disc prefab w/ Scripts
    Prefab for the moving disc and the scripts which control the movement, and spawning locations

---
+++ DiscMovment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script that defines the Move speed of the Discs
+public class Disc : MonoBehaviour {
+	public float speed = 10.0f;//default move speed
+	private Rigidbody2D rb;
+
+    void Start(){
+		rb = this.GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2(-speed, 0);//sets speed of disc to oposite of speed (eg -10)
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Spawndiscs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This should go on Main Camera in Unity
+//script which Spawns discs at points just outside of the screenbounds
+//
+
+public class Spawndiscs : MonoBehaviour{
+    public GameObject discPrefab;//loads discPrefab into code
+    public float SpawnTime = 1.0f;//Default spawn time
+    private Vector2 screenBounds;//Finds screen size
+
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(DiscSpawns());
+    }
+    public void SpawnDiscs () //Method which spawns Discs
+    {
+        GameObject a = Instantiate(discPrefab) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x + 8, Random.Range(-screenBounds.y, screenBounds.y)); //Spawns discs at random y value
+    }
+    IEnumerator DiscSpawns()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(SpawnTime);
+            SpawnDiscs();
+        }
+    }
+}
