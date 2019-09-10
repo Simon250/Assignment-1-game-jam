bcdfebd6fc2f8d7c90717b33f4663f9afc9f2873
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-09-10 13:39:28 -0400

    god dam i finaly got it to work

--- Game Jam (Movement)/Assets/Scripts/ScoreBar.cs
+++ Game Jam (Movement)/Assets/Scripts/ScoreBar.cs
@@ -2,45 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBar : MonoBehaviour
+public class ScoreBar : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int curHealth = 100;
+    public int nextlevel = 100;
+    public int curPoints = 0;
 
     public Texture2D bgImage;
     public Texture2D fgImage;
 
-    public float healthBarLength;
+    public float scoreBarLength;
 
     // Use this for initialization
     void Start()
     {
-        healthBarLength = Screen.width / 2;
+        scoreBarLength = Screen.width / 2;
     }
 
-    public int damage = 0;
+    public int points = 0;
     // Update is called once per frame
     void Update()
     {
-        AddjustCurrentHealth(damage);
-        damage = 0;
+        AddjustCurrentHealth(points);
+        points = 0;
     }
 
     void OnGUI()
     {
         // Create one Group to contain both images
         // Adjust the first 2 coordinates to place it somewhere else on-screen
-        GUI.BeginGroup(new Rect(0, 0, healthBarLength, 32));
+        GUI.BeginGroup(new Rect(Screen.width/2, 32, scoreBarLength, 32));
 
         // Draw the background image
-        GUI.Box(new Rect(0, 0, healthBarLength, 32), bgImage);
+        GUI.Box(new Rect(Screen.width / 2, 32, scoreBarLength, 32), bgImage);
 
         // Create a second Group which will be clipped
         // We want to clip the image and not scale it, which is why we need the second Group
-        GUI.BeginGroup(new Rect(0, 0, curHealth / maxHealth * healthBarLength, 32));
+        GUI.BeginGroup(new Rect(Screen.width/2, 32, curPoints / nextlevel * scoreBarLength, 32));
 
         // Draw the foreground image
-        GUI.Box(new Rect(0, 0, healthBarLength, 32), fgImage);
+        GUI.Box(new Rect(Screen.width / 2, 32, scoreBarLength, 32), fgImage);
 
         // End both Groups
         GUI.EndGroup();
@@ -51,14 +51,11 @@ public class HealthBar : MonoBehaviour
     public void AddjustCurrentHealth(int adj)
     {
 
-        curHealth += adj;
+        curPoints += adj;
 
-        if (curHealth < 0)
-            curHealth = 0;
+        if (curPoints < 0)
+            curPoints = 0;
 
-        if (maxHealth < 1)
-            maxHealth = 1;
-
-        healthBarLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
+        scoreBarLength = (Screen.width / 2) * (curPoints / (float)nextlevel);
     }
 }
