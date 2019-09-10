48d25f0c1f5b9d7f29b55853fb849777fa7c871c
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-09-09 22:24:41 -0400

    making shure everything works

--- Game Jam (Movement)/Assets/PlayerMovement.cs
+++ Game Jam (Movement)/Assets/PlayerMovement.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
+    private Vector2 screenBounds;//Finds screen size
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = 6.0f;
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.position += move * speed * Time.deltaTime;
+        // tests if position is out of the top boundry
+        if (transform.position.y > (screenBounds.y))
+        {
+            transform.position = transform.position - move;
+        }
+        // tests if position is out of the bottom boundry
+        else if (transform.position.y < (-screenBounds.y))
+        {
+            transform.position = transform.position - move;
+        }
+        // tests if position is out of the right boundry (i think its right)
+        else if (transform.position.x > (screenBounds.x))
+        {
+            transform.position = transform.position - move;
+        }
+        // tests if position is out of the left boundry
+        else if (transform.position.x < (-screenBounds.x))
+        {
+            transform.position = transform.position - move;
+        }
+        else
+        {
+            transform.position += move * speed * Time.deltaTime;
+        }
     }
 }
