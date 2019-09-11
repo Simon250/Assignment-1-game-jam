593e63f464b1753353cee6977736d630ad0e5f13
Joseph Smyth <josephsmyth1@hotmail.com>
2019-09-11 13:23:57 -0400

    Control menu

--- Game Jam (Movement)/Assets/Scripts/SceneSwitch.cs
+++ Game Jam (Movement)/Assets/Scripts/SceneSwitch.cs
@@ -7,6 +7,17 @@ public class SceneSwitch : MonoBehaviour
 {
     public void GotoMainScene()
     {
+        Debug.Log("Loaded Game");
         SceneManager.LoadScene("PlayerMoveTest");
+    }
+    public void GotoMainMenu()
+    {
+        Debug.Log("Loaded Main Menu");
+        SceneManager.LoadScene("Main Menu GUI");
+    }
+    public void GotoControls()
+    {
+        Debug.Log("Loaded Controls");
+        SceneManager.LoadScene("ControlsMM");
     }
 }
