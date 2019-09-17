db3079eb3e3c9e81a48577d8c6f423f07285d37a
Joseph Smyth <josephsmyth1@hotmail.com>
2019-09-17 13:14:08 -0400

    ddddd

--- Game Jam (Movement)/Assets/Scripts/DiscMove.cs
+++ Game Jam (Movement)/Assets/Scripts/DiscMove.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class DiscMove : MonoBehaviour
 {
     public float speed = 20.0f;//default move speed
-    private Rigidbody rb;
-    private Vector2 screenBounds;
+    private Rigidbody rb;//gets the regib body settings for movement
+    private Vector2 screenBounds;//Gtes the size of the screen.
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
-        rb.velocity = new Vector2(-speed, 0);//sets speed of disc to oposite of speed (eg -10)
+        rb.velocity = new Vector2(-speed, 0);//sets speed of disc to oposite of speed
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     void Update()
-    {
+        {
         if (transform.position.x < screenBounds.x-35|| transform.position.x > screenBounds.x + 50 || transform.position.y < screenBounds.y - 12 || transform.position.y > screenBounds.y + 2 )
         {
             Destroy(this.gameObject);//Destroys the disc if out of bounds
